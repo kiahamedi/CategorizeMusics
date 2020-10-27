f9810e7b849ee16989e035f18bed37dea8422c4f
Nasim Gholizadeh <nasim.2000ngh@gmail.com>
2020-10-27 15:49:01 +0330

    create a folder for tracks that do not have an artist name.

--- script.sh
+++ script.sh
@@ -4,13 +4,13 @@ files=`ls *mp3`
 for i in $files; do
     artist=$( mp3info -p "%a" $i )
     if [ "$artist" ];then
-    echo $i
-    mkdir -p "$artist"
-    mv "$i" "$artist"
-elif [ -d "other" ];then
-	mv "$i" "other"
-else
-	mkdir "other"
-	mv "$i" "other"
+         echo $i
+         mkdir -p "$artist"
+         mv "$i" "$artist"
+    elif [ -d "other" ];then
+        mv "$i" "other"
+    else
+    	mkdir "other"
+    	mv "$i" "other"
     fi
 done
